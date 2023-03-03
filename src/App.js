55ba18a22b02730b214be1570552dd9a9f608c53
Klein <escutonklein@gmail.com>
2023-03-03 22:36:02 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Modal from './Modal';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // parent component
+    return (
+        <div className='App'>
+            {/* child component */}
+            <Navbar />
+            {/* <Modal message='Are you sure?' /> */}
+        </div>
+    );
 }
 
 export default App;
